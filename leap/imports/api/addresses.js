fe585177ec30d7db7def6bfb1a983a3cea402681
Dana Tran <dana.l.tran@gmail.com>
2017-01-11 22:27:29 -0800

    calcDistance function works, needs clean up with display and styling

--- leap/imports/api/addresses.js
+++ leap/imports/api/addresses.js
@@ -15,34 +15,34 @@ if (Meteor.isServer) {
 Meteor.methods({
 
   'addresses.calcDistance': function(latInput, lngInput, radiusInput){
-  	// check(latInput, Number);
-  	// check(lngInput, Number);
-  	// check(radiusInput, Number);
+  	check(parseInt(latInput), Number);
+  	check(parseInt(lngInput), Number);
+  	check(parseInt(radiusInput), Number);
   	// check(addressId, String);
 
     if(!latInput || !lngInput || !radiusInput){
       throw new Meteor.Error("need all inputs");
     }
+    //TODO: reset all shouldDisplay values to true
+    Addresses.update({}, {$set: {'shouldDisplay':true}}, {multi: true});
 
     //apply the bottom to every document in the Addresses collection
     var addressCollection = Addresses.find({}).fetch();
     addressCollection.forEach(function(address){
-      console.log('inside Addresses.find');
       // var address = Addresses.findOne(AddressId);
       var latFromDB = address.latitude;
       var lngFromDB = address.longitude;
-      console.log('latfromDb:', latFromDB);
 
       //convert degrees to radians
       var latInputInRadians = latInput * (Math.PI / 180);
       var lngInputInRadians = lngInput * (Math.PI / 180);
       var latFromDBInRadians = latFromDB * (Math.PI / 180);
       var lngFromDBInRadians = lngFromDB * (Math.PI / 180);
-      console.log('latInputInRadians:',latInputInRadians, 'lngInputInRadians:',lngInputInRadians, 'latFromDBInRadians:',latFromDBInRadians, 'lngFromDBInRadians:', lngFromDBInRadians);
 
       var deltaLambda = Math.abs(lngInputInRadians - lngFromDBInRadians);
-      console.log('deltaLambda', deltaLambda);
-      var centralAngle = Math.acos(Math.sin(latInputInRadians) * Math.sin(latFromDBInRadians) + Math.cos(latInputInRadians) + Math.cos(latFromDBInRadians) * Math.cos(deltaLambda));
+      
+      //Math.acos returns NAN if its parameter is outside the range -1 to 1
+      var centralAngle = Math.acos(((Math.sin(latInputInRadians) * Math.sin(latFromDBInRadians))) + (Math.cos(latInputInRadians) * Math.cos(latFromDBInRadians) * Math.cos(deltaLambda)));
       console.log('centralAngle:', centralAngle);
       var radiusOfEarth = 3959; //3,959 miles
       var distance = radiusOfEarth * centralAngle;
@@ -50,7 +50,7 @@ Meteor.methods({
       console.log('distance:', distance, 'radiusInput:', radiusInput);
       if (distance > radiusInput){
         console.log('this address is outside the radius bounds');
-        Addresses.update({latitude: latFromDB, longitude: lngFromDB}, {$set: {'shouldDisplay': false}});  //how to select specific document?
+        Addresses.update({latitude: latFromDB, longitude: lngFromDB}, {$set: {'shouldDisplay': false}});
       }
     });
 
