d3938afce12b5433d2e21568dc55ac78ac8aa451
Dana Tran <dana.l.tran@gmail.com>
2017-01-18 00:12:13 -0800

    refactored form, validation message appears, still needs clean up

--- leap/imports/api/addresses.js
+++ leap/imports/api/addresses.js
@@ -15,42 +15,53 @@ if (Meteor.isServer) {
 Meteor.methods({
 
   'addresses.calcDistance': function(addressInput, latInput, lngInput, radiusInput){
-  	check(addressInput, String);
+  	
     check(+latInput, Number);
   	check(+lngInput, Number);
   	check(+radiusInput, Number);
 
+    
     //TODO: need to display error messages for input validation to UI - choice between address or lat/lng
-    if(!latInput || !lngInput || !radiusInput){
-      throw new Meteor.Error("need all inputs");
-    }
-    //reset all shouldDisplay values to false
-    Addresses.update({}, {$set: {'shouldDisplay':false}}, {multi: true});
-
-    //apply the bottom to every document in the Addresses collection
-    let addressCollection = Addresses.find({}).fetch();
-    addressCollection.forEach(function(address){
-
-      let latFromDB = address.latitude;
-      let lngFromDB = address.longitude;
-
-      //convert degrees to radians
-      let latInputInRadians = latInput * (Math.PI / 180);
-      let lngInputInRadians = lngInput * (Math.PI / 180);
-      let latFromDBInRadians = latFromDB * (Math.PI / 180);
-      let lngFromDBInRadians = lngFromDB * (Math.PI / 180);
-
-      let deltaLambda = Math.abs(lngInputInRadians - lngFromDBInRadians);
+    // console.log('!latInput:', !latInput);
+    // if(!latInput || !lngInput || !radiusInput){
       
-      //Math.acos returns NAN if its parameter is outside the range -1 to 1
-      let centralAngle = Math.acos(((Math.sin(latInputInRadians) * Math.sin(latFromDBInRadians))) + (Math.cos(latInputInRadians) * Math.cos(latFromDBInRadians) * Math.cos(deltaLambda)));
-      let radiusOfEarth = 3959; //3,959 miles
-      let distance = radiusOfEarth * centralAngle;
+    //   this.setState({
+    //     inputValidationMessage: 'need inputs dude',
+    //   });
 
-      if (distance < radiusInput){
-        Addresses.update({latitude: latFromDB, longitude: lngFromDB}, {$set: {'shouldDisplay': true}});
-      }
-    });
+    // } else {
+
+      //reset all shouldDisplay values to false
+      Addresses.update({}, {$set: {'shouldDisplay':false}}, {multi: true});
+
+      //TODO: if address was provided, convert that to a lat/lng
+
+      //apply the bottom to every document in the Addresses collection
+      let addressCollection = Addresses.find({}).fetch();
+      addressCollection.forEach(function(address){
+
+        let latFromDB = address.latitude;
+        let lngFromDB = address.longitude;
+
+        //convert degrees to radians
+        let latInputInRadians = latInput * (Math.PI / 180);
+        let lngInputInRadians = lngInput * (Math.PI / 180);
+        let latFromDBInRadians = latFromDB * (Math.PI / 180);
+        let lngFromDBInRadians = lngFromDB * (Math.PI / 180);
+
+        let deltaLambda = Math.abs(lngInputInRadians - lngFromDBInRadians);
+        
+        //Math.acos returns NAN if its parameter is outside the range -1 to 1
+        let centralAngle = Math.acos(((Math.sin(latInputInRadians) * Math.sin(latFromDBInRadians))) + (Math.cos(latInputInRadians) * Math.cos(latFromDBInRadians) * Math.cos(deltaLambda)));
+        let radiusOfEarth = 3959; //3,959 miles
+        let distance = radiusOfEarth * centralAngle;
+
+        if (distance < radiusInput){
+          Addresses.update({latitude: latFromDB, longitude: lngFromDB}, {$set: {'shouldDisplay': true}});
+        }
+      });
+    // }
+
 
   },
 });
@@ -60,7 +71,7 @@ if (Meteor.isClient) {
     'submit form': function(event){
       event.preventDefault();
 
-      let addressInput = event.target.addressInput.value;
+      let addressInput = event.target.addressInput.value || '';
       let latInput = event.target.latInput.value;
       let lngInput = event.target.lngInput.value;
       let radiusInput = event.target.radiusInput.value;
--- leap/imports/ui/App.jsx
+++ leap/imports/ui/App.jsx
@@ -9,6 +9,58 @@ import Address from './Address.jsx';
  
 // App component - represents the whole app
 class App extends Component {
+  constructor(props) {
+    super(props);
+ 
+    this.state = {
+      inputValidationMessage: '',
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+
+  }
+  
+  handleSubmit(event) {
+    event.preventDefault();
+
+    // Find the text field via the React ref
+    const addressInput = ReactDOM.findDOMNode(this.refs.addressInput).value.trim();
+    const latInput = ReactDOM.findDOMNode(this.refs.latInput).value.trim();
+    const lngInput = ReactDOM.findDOMNode(this.refs.lngInput).value.trim();
+    const radiusInput = ReactDOM.findDOMNode(this.refs.radiusInput).value.trim();
+    console.log('addressInput: ',addressInput);
+    if(addressInput || (latInput && lngInput)){
+      this.setState({
+        inputValidationMessage: '',
+      });
+
+      Meteor.call('addresses.calcDistance', addressInput, latInput, lngInput, radiusInput);
+    } else {
+      this.setState({
+        inputValidationMessage: 'need address dude',
+      });
+      //TODO: clear previous output results
+    }
+    // Clear form
+    ReactDOM.findDOMNode(this.refs.addressInput).value = '';
+    ReactDOM.findDOMNode(this.refs.latInput).value = '';
+    ReactDOM.findDOMNode(this.refs.lngInput).value = '';
+    ReactDOM.findDOMNode(this.refs.radiusInput).value = '';
+  }
+
+  // validateForm() {
+  //   // Find the text field via the React ref
+  //   const addressInput = ReactDOM.findDOMNode(this.refs.addressInput).value.trim();
+  //   console.log('addressInput: ',addressInput);
+  //   if(!addressInput){
+  //     this.setState({
+  //       inputValidationMessage: 'need address dude',
+  //     });
+  //   } else {
+  //     this.setState({
+  //       inputValidationMessage: '',
+  //     });
+  //   }
+  // }
 
   renderAddresses() {
     let filteredAddresses = this.props.addresses;
@@ -28,8 +80,17 @@ class App extends Component {
         <header>
           <h1>leap</h1>
         </header>
-          <p>Enter a radius and your choice of an address or latitude/longitude coordinates below to see a list of addresses within the radius.</p>
-
+        <p>Enter a radius and your choice of an address or latitude/longitude coordinates below to see a list of addresses within the radius.</p>
+        <div id="formValidation">{this.state.inputValidationMessage}</div>
+          <form onSubmit={this.handleSubmit.bind(this)}>
+            <input type = "text" name = "addressInput" placeholder="address" className="addressInput" ref="addressInput" />
+            <br />
+            <input type = "text" name = "latInput" placeholder="latitude in render" ref="latInput" />
+            <input type = "text" name = "lngInput" placeholder="longitude" ref="lngInput" />
+            <input type = "text" name = "radiusInput" placeholder="radius (in miles)*" ref="radiusInput" required />
+            <br />
+            <input type = "submit" value = "submit" className="submitButton" />
+          </form>
         <ul>
           {this.renderAddresses()}
         </ul>
